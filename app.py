a1ada785ce67bb03eeedafef069182d6a7dc50c0
gheyderov <g.heyderov@gmail.com>
2023-02-07 20:22:24 +0400

    product

--- app.py
+++ app.py
@@ -2,20 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-users = {
-    'user_1' : {
+products = {
+    1 : {
         'id' : 1,
-        'name' : 'John',
-        'surname' : 'Doe'
+        'name' : 'Product #1',
+        'description' : 'some description',
+        'price' : 100,
+        'image' : 'p1.avif'
     },
-    'user_2' : {
+    2 : {
         'id' : 2,
-        'name' : 'Smith',
-        'surname' : 'Doe'
+        'name' : 'Product #2',
+        'description' : 'some description',
+        'price' : 200,
+        'image' : 'p2.avif'
+    },
+    3 : {
+        'id' : 3,
+        'name' : 'Product #3',
+        'description' : 'some description',
+        'price' : 300,
+        'image' : 'p3.avif'
     }
 }
 
+colors = ['red', 'blue', 'green']
 
 @app.route("/home")
 def home():
-    return render_template('index.html', students = users)
+    return render_template('index.html', products = products)
+
+
+@app.route("/product/<int:id>")
+def product(id):
+    return render_template('product.html', product = products[id], colors = colors)
