1a4f1bfbc638dd8303bda0e4d948be301159ec96
gheyderov <g.heyderov@gmail.com>
2023-02-14 20:41:21 +0400

    forms

--- controllers.py
+++ controllers.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
-from models import Product
+from flask import render_template, request
+from models import Product, Contact, Category
+from forms import ContactForm
 
 
 
@@ -8,10 +9,37 @@ from models import Product
 @app.route("/home")
 def home():
     items = Product.query.all()
-    return render_template('index.html', products = items)
+    categories = Category.query.all()
+    return render_template('index.html', products = items, categories = categories)
+
+
+@app.route("/category/<string:name>")
+def cat(name):
+    items = Category.query.filter_by(name = name).first().product
+    categories = Category.query.all()
+    return render_template('index.html', products = items, categories = categories)
+
+
 
 
 @app.route("/product/<int:id>")
 def product(id):
     item = Product.query.filter_by(id = id).first()
     return render_template('product.html', product = item)
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.form)
+        if form.validate_on_submit():
+            contact = Contact(
+                name = form.name.data,
+                email = form.email.data,
+                company = form.company.data,
+                message = form.message.data,
+                is_subscribe = form.is_subscribe.data
+            )
+            contact.save()
+    return render_template('contact.html', form = form)
--- models.py
+++ models.py
@@ -37,22 +37,65 @@ class Blog(db.Model):
         db.session.commit()
 
 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    company = db.Column(db.String(100))
+    message = db.Column(db.String(255))
+    is_subscribe = db.Column(db.Boolean)
+
+    def __init__(self, name, email, company, message, is_subscribe):
+        self.name = name
+        self.email = email
+        self.company = company
+        self.message = message
+        self.is_subscribe = is_subscribe
+
+    def __repr__(self):
+        return self.email
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+
+
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     description = db.Column(db.String(100), nullable = False)
     price = db.Column(db.Integer, nullable = False)
     image = db.Column(db.String(100), nullable = False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable = False)
 
 
-    def __init__(self, title, description, price, image):
+    def __init__(self, title, description, price, image, category_id):
         self.title = title
         self.description = description
         self.price = price
         self.image = image
+        self.category_id = category_id
 
     def __repr__(self):
         return self.title
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    product = db.relationship('Product', backref = 'category')
+
+    def __init__(self, name) -> None:
+        self.name = name
+        
+    def __repr__(self):
+        return self.name
 
     def save(self):
         db.session.add(self)
